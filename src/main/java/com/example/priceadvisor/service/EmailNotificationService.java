fb2989c1e343594bc3962ed86a079f666234bcdd
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-12-06 02:41:49 -0500

    Fixed up SNS so that It can build the Arn on its own, and now it properly can subscribe and unsubscribe.

--- src/main/java/com/example/priceadvisor/service/EmailNotificationService.java
+++ src/main/java/com/example/priceadvisor/service/EmailNotificationService.java
@@ -1,6 +1,7 @@
 package com.example.priceadvisor.service;
 
 import com.example.priceadvisor.entity.User;
+import com.example.priceadvisor.repository.BusinessRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sns.SnsClient;
@@ -9,16 +10,25 @@ import software.amazon.awssdk.services.sns.model.*;
 @Service
 public class EmailNotificationService {
     private final SnsClient snsClient;
-    @Value("${aws.sns.topic.arn}") // Inject the topic ARN from the application.properties file.
-    private String topicArn;    //ToDo: change to the actual topic ARN based off of that businesses topic and frequency of the user
-//    private String topicArn  = "arn:aws:sns:us-east-1:471112717872:Test_Business_DAILY"; //for testing purposes
+    private final BusinessRepository businessRepository;
 
-    public EmailNotificationService(SnsClient snsClient) {
+    @Value("${aws.sns.base.arn}") // Inject the topic ARN from the application.properties file.
+    private String baseArn;
+    private String topicArn;
+
+
+    public EmailNotificationService(SnsClient snsClient, BusinessRepository businessRepository) {
         this.snsClient = snsClient;
+        this.businessRepository = businessRepository;
     }
 
-    public void subscribe(String userEmail, User.EmailNotificationsFrequency emailNotificationsFrequency) {
+    public void subscribe(String userEmail, User.EmailNotificationsFrequency emailNotificationsFrequency, int businessId) {
         try {
+            String businessName = businessRepository.findById(businessId)
+                    .orElseThrow(() -> new IllegalArgumentException("Business not found.")).getName();
+
+            String topicArn = buildTopicArn(emailNotificationsFrequency, businessName);
+
             // Subscribe to the topic
             SubscribeRequest request = SubscribeRequest.builder()
                     .protocol("email")
@@ -28,19 +38,30 @@ public class EmailNotificationService {
                     .build();
 
             snsClient.subscribe(request);
-//            SubscribeResponse result = snsClient.subscribe(request);              //for testing purposes
-//            System.out.println("Subscription ARN: " + result.subscriptionArn()); //for testing purposes
+            publishNotification("You have successfully subscribed to Price Advisor Notifications great work", userEmail, emailNotificationsFrequency, businessId); //for testing purposes to make sure that publish method works
+
         } catch (SnsException e) {
             System.err.println("Error Subscribing: " + e.getMessage());
         }
 
-
     }
 
-    public void unsubscribe(String userEmail) {
-        publishNotification("You have successfully subscribed to Price Advisor Notifications great work"); //for testing purposes to make sure that publish method works
+    private String buildTopicArn(User.EmailNotificationsFrequency emailNotificationsFrequency, String businessName) {
+        String formattedBusinessName = businessName.replaceAll("\\s+", "_");
+        String topicName = formattedBusinessName + "_" + emailNotificationsFrequency.name();
+        String topicArn = baseArn + topicName;
+        return topicArn;
+    }
 
+    public void unsubscribe(String userEmail,User.EmailNotificationsFrequency emailNotificationsFrequency, int businessId) {
         try {
+            String businessName = businessRepository.findById(businessId)
+                    .orElseThrow(() -> new IllegalArgumentException("Business not found.")).getName();
+
+
+            String topicArn = buildTopicArn(emailNotificationsFrequency, businessName);
+
+
             // List all subscriptions for the topic and find the subscription to unsubscribe from
             ListSubscriptionsByTopicRequest listRequest = ListSubscriptionsByTopicRequest.builder()
                     .topicArn(topicArn)
@@ -65,8 +86,15 @@ public class EmailNotificationService {
 
     }
 
-    public void publishNotification(String message) {
+    public void publishNotification(String message, String userEmail, User.EmailNotificationsFrequency emailNotificationsFrequency, int businessId) {
         try {
+            String businessName = businessRepository.findById(businessId)
+                    .orElseThrow(() -> new IllegalArgumentException("Business not found.")).getName();
+
+
+            String topicArn = buildTopicArn(emailNotificationsFrequency, businessName);
+
+
             PublishRequest request = PublishRequest.builder()
                     .message(message)
                     .topicArn(topicArn)
@@ -79,5 +107,59 @@ public class EmailNotificationService {
     }
 
 
-
+//    public void updateNotificationSettings(String email, User.EmailNotificationsFrequency emailNotificationFrequency) {
+//        User user = userRepository.findByEmail(email)
+//                .orElseThrow(() -> new IllegalArgumentException("User not found."));
+//        user.setEmailNotificationsFrequency(emailNotificationFrequency);
+//        userRepository.save(user);
+//
+//        subscribeToSNSTopic(email, emailNotificationFrequency);
+//    }
+//
+//    private void subscribeToSNSTopic(String email, User.EmailNotificationsFrequency emailNotificationFrequency) {
+//
+//
+//        if (emailNotificationFrequency == User.EmailNotificationsFrequency.NONE)
+//            return;
+//
+//        String topicArn;
+//        switch (emailNotificationFrequency) {
+//            case DAILY:
+//                topicArn = "arn:aws:sns:us-east-1:471112717872:Test_Business_DAILY";
+//                break;
+//            case HOURLY:
+//                topicArn = "arn:aws:sns:us-east-1:471112717872:Test_Business_HOURLY\n";
+//                break;
+//            case WEEKLY:
+//                topicArn = "arn:aws:sns:us-east-1:471112717872:Test_Business_MONTHLY\n";
+//                break;
+//            case MONTHLY:
+//                topicArn = "arn:aws:sns:us-east-1:471112717872:Test_Business_WEEKLY\n";
+//                break;
+//            default:
+//                throw new IllegalArgumentException("Invalid email notification frequency: " + emailNotificationFrequency + ". Enter valid information");
+//        }
+//
+//        SnsClient snsClient = SnsClient.builder()
+//                .region(Region.US_EAST_1)
+//                .credentialsProvider(ProfileCredentialsProvider.create())
+//                .build();
+//
+//
+//        try {
+//            SubscribeRequest request = SubscribeRequest.builder()
+//                    .protocol("email")
+//                    .endpoint(email)
+//                    .returnSubscriptionArn(true)
+//                    .topicArn(topicArn)
+//                    .build();
+//
+//            SubscribeResponse response = snsClient.subscribe(request);
+//            System.out.println("Subscription ARN: " + response.subscriptionArn());
+//            System.out.println("Subscription pending. Confirm via the email sent to " + email);
+//        } catch (
+//                SnsException e) {
+//            System.err.println(e.awsErrorDetails().errorMessage());
+//        }
+//    }
 }
--- src/main/java/com/example/priceadvisor/service/UserService.java
+++ src/main/java/com/example/priceadvisor/service/UserService.java
@@ -1,23 +1,32 @@
 package com.example.priceadvisor.service;
 
+import com.example.priceadvisor.entity.Business;
 import com.example.priceadvisor.entity.User;
+import com.example.priceadvisor.repository.BusinessRepository;
 import com.example.priceadvisor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Value("${aws.sns.base.arn}") // Inject the base ARN from the application.properties file.
+    private String baseArn;
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final SecurityContextService securityContextService; // Injected service
+    public final BusinessRepository BusinessRepository;
+
+
 
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, SecurityContextService securityContextService) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, SecurityContextService securityContextService, BusinessRepository BusinessRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.securityContextService = securityContextService;
+        this.BusinessRepository = BusinessRepository;
     }
 
     // Add a new user with hashed password
@@ -41,11 +50,20 @@ public class UserService {
         return user.getEmailNotificationsFrequency();
     }
 
-    public void setCurrentEmailNotificationsFrequency(Integer userId, User.EmailNotificationsFrequency emailFrequency) {
+    public String setCurrentEmailNotificationsFrequency(Integer userId, User.EmailNotificationsFrequency emailFrequency, BusinessRepository businessRepository) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         user.setEmailNotificationsFrequency(emailFrequency);
         userRepository.save(user);
+
+        int businessId = user.getBusinessId();
+
+        String businessName = BusinessRepository.findById(businessId)
+                .orElseThrow(() -> new IllegalArgumentException("Business not found"))
+                .getName();
+
+        String frequency = user.getEmailNotificationsFrequency().name();
+        return String.format("%s%s_%s", baseArn, businessName, frequency);
     }
 
     public Integer getCurrentBusinessId() {
