aca300a216ca88c7de330a60454d8172918b8a3b
a-edel <ayemail18@gmail.com>
2024-12-06 00:59:24 -0500

    Added getters to FetchedData.java

--- src/main/java/com/example/priceadvisor/datafetching/FetchedData.java
+++ src/main/java/com/example/priceadvisor/datafetching/FetchedData.java
@@ -3,16 +3,29 @@ package com.example.priceadvisor.datafetching;
 import java.math.BigDecimal;
 
 public class FetchedData {
+
     private BigDecimal amazonPrice;
     private BigDecimal walmartPrice;
     private BigDecimal ebayPrice;
+
+    public BigDecimal getAmazonPrice() {
+        return amazonPrice;
+    }
 
     public void setAmazonPrice(BigDecimal amazonPrice) {
         this.amazonPrice = amazonPrice;
     }
 
+    public BigDecimal getWalmartPrice() {
+        return walmartPrice;
+    }
+
     public void setWalmartPrice(BigDecimal walmartPrice) {
         this.walmartPrice = walmartPrice;
+    }
+
+    public BigDecimal getEbayPrice() {
+        return ebayPrice;
     }
 
     public void setEbayPrice(BigDecimal ebayPrice) {
