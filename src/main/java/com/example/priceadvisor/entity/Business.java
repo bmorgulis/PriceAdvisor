4183a60e24ccb770beba7d79513f01c7ac300231
a-edel <ayemail18@gmail.com>
2024-12-05 23:30:02 -0500

    Created Business entity

---
+++ src/main/java/com/example/priceadvisor/entity/Business.java
@@ -0,0 +1,35 @@
+package com.example.priceadvisor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "businesses")
+public class Business {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "businessId")
+    private Integer businessId;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    // Default constructor needed for JPA
+    public Business() {}
+
+    public Business(String name) {
+        this.name = name;
+    }
+
+    public Integer getBusinessId() {
+        return businessId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
