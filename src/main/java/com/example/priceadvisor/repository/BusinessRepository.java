e20224186a31f35b029882b02078ead8fd580e8d
a-edel <ayemail18@gmail.com>
2024-12-13 00:01:59 -0500

    Minor changes

--- src/main/java/com/example/priceadvisor/repository/BusinessRepository.java
+++ src/main/java/com/example/priceadvisor/repository/BusinessRepository.java
@@ -3,9 +3,6 @@ package com.example.priceadvisor.repository;
 import com.example.priceadvisor.entity.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 
 public interface BusinessRepository extends JpaRepository<Business, Integer> {
-    Optional<Business> findById(Integer name);
 }
